2b501f9e1b7636e0ca2bd8e9a60efd15cc5044fc
nicknessuno01 <nicknessuno001@gmail.com>
2018-01-02 18:23:35 +0100

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,42 @@
+<?php
+
+
+//code by @nicknessuno01
+//@cancr
+
+
+if($msg == "/start")
+{
+$getid = file_get_contents("DATA/british/british $userID");
+if($getid == "")
+{
+mkdir("DATA");
+mkdir("DATA/british");
+file_put_contents("DATA/british/british $userID", "british");
+$zuccheroconpanna[] = array(
+array(
+"text" => "Canale British!",
+"url" => "https://t.me/BRRITISH"),
+);
+sm($chatID, "ORA SEI BRRITISHHHHHHHHHH🇬🇧🇬🇧🇬🇧", $zuccheroconpanna, 'Markdown', false, false, true);
+
+
+$menu[] = array(
+array(
+"text" => "Diventa British!",
+"url" => "t.me/BrritishBot"),
+);
+sm("-1001264887149", "$nome è un britishhhhh🇬🇧🇬🇧🇬🇧", $menu, 'Markdown', false, false, true);
+sm("-1001357944415", "$nome è un britishhhhh🇬🇧🇬🇧🇬🇧", $menu, 'Markdown', false, false, true);
+exit;
+}
+if($getid == "british")
+{
+$menu[] = array(
+array(
+"text" => "Canale British!",
+"url" => "https://t.me/BRRITISH"),
+);
+sm($chatID, "Sei gia british 🇬🇧🇬🇧🇬🇧", $menu, 'Markdown', false, false, true);
+}
+}
